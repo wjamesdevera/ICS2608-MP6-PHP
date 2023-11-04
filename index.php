6edb5efcafb2d218085ebcc23ead0bfd2db47fa3
Winfrey <w.jamesdevera@gmail.com>
2023-11-04 19:03:43 +0800

    added required for fields

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ include("./templates/header.php");
                     <div class="name form-section grid-span-2">
                         <h4>Full Name</h4>
                         <div class="name-fields">
-                            <input type="text" name="first_name" id="first_name" placeholder="First Name">
-                            <input type="text" name="last_name" id="last_name" placeholder="Last Name">
+                            <input type="text" name="first_name" id="first_name" placeholder="First Name" required>
+                            <input type="text" name="last_name" id="last_name" placeholder="Last Name" required>
                         </div>
                     </div>
                     <div class="gender form-section">
@@ -31,11 +31,11 @@ include("./templates/header.php");
                     </div>
                     <div class="student-id form-section">
                         <label for="student_id">Student No.</label>
-                        <input type="text" name="student_id" id="student_id" placeholder="Student Number">
+                        <input type="text" name="student_id" id="student_id" placeholder="Student Number" required>
                     </div>
                     <div class="year-level form-section">
                         <label for="year_level">Year Level</label>
-                        <select name="year_level" id="year">
+                        <select name="year_level" id="year" required>
                             <option value="" selected disabled>Select Year Level</option>
                             <?php
                             foreach ($yearLevel as $key => $year) {
@@ -47,7 +47,7 @@ include("./templates/header.php");
                     </div>
                     <div class="degree form-section">
                         <label for="degree">Academic Program </label>
-                        <select name="degree" id="degree">
+                        <select name="degree" id="degree" required>
                             <option value="" selected disabled>Select a Program</option>
                             <?php
                             foreach ($cicsDegreeProgam as $key => $program) {
@@ -59,12 +59,12 @@ include("./templates/header.php");
                     </div>
                     <div class="email form-section">
                         <label for="email">Email Address</label>
-                        <input type="email" name="email" id="email" placeholder="Email">
+                        <input type="email" name="email" id="email" placeholder="Email" required>
                     </div>
                     <div class="dob form-section">
                         <h4>Birth Date</h4>
                         <div class="dob-select-section">
-                            <select name="month" id="month">
+                            <select name="month" id="month" required>
                                 <option value="" selected disabled>Month</option>
                                 <?php
                                 foreach ($months as $key => $month) {
@@ -73,7 +73,7 @@ include("./templates/header.php");
                                 }
                                 ?>
                             </select>
-                            <select name="day" id="day">
+                            <select name="day" id="day" required>
                                 <option value="" selected disabled>Day</option>
                                 <?php
                                 for ($i = 1; $i <= 31; $i++) {
@@ -81,7 +81,7 @@ include("./templates/header.php");
                                 }
                                 ?>
                             </select>
-                            <select name="year" id="year">
+                            <select name="year" id="year" required>
                                 <option value="" selected disabled>Year</option>
                                 <?php
                                     foreach($years as $year) {
@@ -93,16 +93,16 @@ include("./templates/header.php");
                     </div>
                     <div class="phone form-section grid-span-2">
                         <label for="phone">Mobile No.</label>
-                        <input type="text" name="phone" id="phone" placeholder="Mobile Number">
+                        <input type="text" name="phone" id="phone" placeholder="Mobile Number" required>
                     </div>
                     <div class="address form-section grid-span-2">
                         <h4>Current Address</h4>
                         <div class="address-fields">
-                            <input type="text" name="street" id="street" placeholder="Street Address">
-                            <input type="text" name="city" id="city" placeholder="City or Locality">
-                            <input type="text" name="state" id="state" placeholder="State, Region, or Province">
-                            <input type="text" name="postal" id="postal" placeholder="Postal Code / ZIP Code">
-                            <select name="country" id="country">
+                            <input type="text" name="street" id="street" placeholder="Street Address" required>
+                            <input type="text" name="city" id="city" placeholder="City or Locality" required>
+                            <input type="text" name="state" id="state" placeholder="State, Region, or Province" required>
+                            <input type="text" name="postal" id="postal" placeholder="Postal Code / ZIP Code" required>
+                            <select name="country" id="country" required>
                                 <option value="" selected disabled>Country</option>
                                 <?php 
                                 foreach($countries as $country) {
